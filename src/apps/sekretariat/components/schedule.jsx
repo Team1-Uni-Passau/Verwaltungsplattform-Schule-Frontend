fd0aadbfe7bda3524efbc37a26e2e453254acc77
hedi <hedifeki@yahoo.fr>
2020-12-17 14:21:27 +0100

    commented secretariat

--- src/apps/sekretariat/components/schedule.jsx
+++ src/apps/sekretariat/components/schedule.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import LeftNavigation from '../../../assets/components/LeftNavigation';
-import '../stylesheets/sekretariat.css';
-import TopBar from '../../../assets/components/topBar';
-import Demo from './Demo';
+// import React from 'react';
+// import LeftNavigation from '../../../assets/components/LeftNavigation';
+// import '../stylesheets/sekretariat.css';
+// import TopBar from '../../../assets/components/topBar';
+// import Demo from './Demo';
   
 
-export default class schedule extends React.Component {
+// export default class schedule extends React.Component {
     
     
-    constructor (props){
-        super(props);
-        this.state = {
-        }
-    }
+//     constructor (props){
+//         super(props);
+//         this.state = {
+//         }
+//     }
     
 
 
-    render() {
+//     render() {
 
-        return (
-            <div className="sekretariat-home">
-                <LeftNavigation selected="Wochenpläne" />
-                <div className="flex-right-container">
-                    <TopBar/>
-                    <div className="middle-panel-container" style={{overflow:'hidden'}}>
-                      <div className="demo">
-                        <Demo/>
-                      </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+//         return (
+//             <div className="sekretariat-home">
+//                 <LeftNavigation selected="Wochenpläne" />
+//                 <div className="flex-right-container">
+//                     <TopBar/>
+//                     <div className="middle-panel-container" style={{overflow:'hidden'}}>
+//                       <div className="demo">
+//                         <Demo/>
+//                       </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
 
 
 
-}
+// }
