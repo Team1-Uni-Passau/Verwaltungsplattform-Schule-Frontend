593b4676dbb4d298b4edbb6cf95c1d1bb84dff2f
hedi <hedifeki@yahoo.fr>
2021-01-02 15:57:22 +0100

    modified watchers limit

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    watchOptions: {
+        ignored: /node_modules/
+    },
     devtool: 'cheap-module-eval-source-map',
     entry: './src/index.js',
     output: {
